d02ea13757080659c3eab26090877e9e1ac99e12
hashyong <564790073@qq.com>
2021-03-11 20:54:43 +0800

    :sparkles: 新增基于无序俩表的符号表

---
+++ search/README.md
@@ -0,0 +1,4 @@
+# 查找算法
+
+## 最最最基础的无序链表版本
+- [SeqSearchST](search.go)
---
+++ search/search.go
@@ -0,0 +1,41 @@
+package search
+
+type Node struct {
+	Key  interface{}
+	Data interface{}
+	Next *Node
+	Prev *Node
+}
+
+// 无序链表
+type SeqSearchST struct {
+	Head *Node
+}
+
+func InitNode(key interface{}, data interface{}) *SeqSearchST {
+	res := &Node{
+		Key:  key,
+		Data: data,
+		Next: nil,
+		Prev: nil,
+	}
+
+	table := &SeqSearchST{
+		Head: res,
+	}
+	return table
+}
+
+// 无序链表, 查找只能挨个遍历
+func (c *SeqSearchST) Get(key interface{}) interface{} {
+	node := c.Head
+	for node != nil {
+		if node.Key == key {
+			return node.Data
+		}
+
+		node = (*node).Prev
+	}
+
+	return nil
+}
