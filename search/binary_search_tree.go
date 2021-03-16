3b356b034ac1d901ba07c9b1785d99101fde89ca
hashyong <564790073@qq.com>
2021-03-16 15:20:50 +0800

    :sparkles: BST支持 select， rank， floor等方法

--- search/binary_search_tree.go
+++ search/binary_search_tree.go
@@ -37,6 +37,89 @@ func (b *BST) Put(key BSSTI, value interface{}) {
 	b.Root = put(b.Root, key, value)
 }
 
+func (b *BST) Min() BSSTI {
+	return min(b.Root).Key
+}
+
+func (b *BST) Floor(key BSSTI) BSSTI {
+	x := floor(b.Root, key)
+	if x == nil {
+		return nil
+	}
+	return x.Key
+}
+
+// 返回排名为k的键, 即树中正好有k个键小于它
+func (b *BST) Select(k int) BSSTI {
+	return treeSelect(b.Root, k)
+}
+
+// 返回指定键的排名, 注意下标从0开始
+func (b *BST) Rank(key BSSTI) int {
+	return rank(b.Root, key)
+}
+
+func rank(node *TreeNode, key BSSTI) int {
+	if node == nil {
+		return 0
+	}
+
+	cmp := key.Compare(node.Key)
+	// key < node.key
+	if cmp < 0 {
+		return rank(node.Left, key)
+	}
+
+	if cmp > 0 {
+		return 1 + size(node.Left) + rank(node.Right, key)
+	}
+
+	return size(node.Left)
+}
+
+func treeSelect(node *TreeNode, k int) BSSTI {
+	if node == nil {
+		return nil
+	}
+
+	t := size(node.Left)
+	if t > k {
+		return treeSelect(node.Left, k)
+	}
+	if t < k {
+		// -1是因为当前节点也不符合预期， 一并减掉
+		return treeSelect(node.Right, k-t-1)
+	}
+
+	return node.Key
+}
+
+func floor(node *TreeNode, key BSSTI) *TreeNode {
+	if node == nil {
+		return nil
+	}
+
+	cmp := key.Compare(node.Key)
+	if cmp == 0 {
+		return node
+	}
+
+	if cmp < 0 {
+		return floor(node.Left, key)
+	}
+
+	return floor(node.Right, key)
+
+}
+
+func min(node *TreeNode) *TreeNode {
+	if node.Left == nil {
+		return node
+	}
+
+	return min(node.Left)
+}
+
 func put(node *TreeNode, key BSSTI, value interface{}) *TreeNode {
 	if node == nil {
 		return &TreeNode{
